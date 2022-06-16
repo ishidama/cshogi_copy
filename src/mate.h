9c50d52bd49bc048104a489b33ea45aef97f2b98
ishidama <ishimada0327@gmail.com>
2022-06-16 14:17:22 +0900

    [fix] ビルドエラー対応
          clang -Wno-unused-result -Wsign-compare -Wunreachable-code -fno-common -dynamic -DNDEBUG -g -fwrapv -O3 -Wall -isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX12.sdk -DHAVE_SSE4 -DHAVE_SSE42 -DHAVE_AVX2 -Isrc -I/Users/ishimada/projects/cshogi_copy/venv/include -I/usr/local/opt/python@3.9/Frameworks/Python.framework/Versions/3.9/include/python3.9 -I/Users/ishimada/projects/cshogi_copy/venv/lib/python3.9/site-packages/numpy/core/include -c src/mate.cpp -o build/temp.macosx-12-x86_64-cpython-39/src/mate.o -msse4.2 -mavx2 -std=c++11
          In file included from src/mate.cpp:1:
          src/position.hpp:250:73: warning: non-void function does not return a value [-Wreturn-type]
                  bool set(const HuffmanCodedPos& hcp) { set_hcp((const char*)hcp.data); };
                                                                                         ^
          src/position.hpp:252:74: warning: non-void function does not return a value [-Wreturn-type]
                  bool set(const PackedSfen& psfen) { set_psfen((const char*)psfen.data); };
                                                                                          ^
          src/mate.cpp:110:26: error: template parameter redefines default argument
          template <bool INCHECK = false>
                                   ^
          src/mate.h:6:26: note: previous default template argument defined here
          template <bool INCHECK = false>
                                   ^
          src/mate.cpp:157:26: error: template parameter redefines default argument
          template <bool INCHECK = false>
                                   ^
          src/mate.h:10:26: note: previous default template argument defined here
          template <bool INCHECK = false>
                                   ^
          2 warnings and 2 errors generated.
          error: command '/usr/bin/clang' failed with exit code 1

--- src/mate.h
+++ src/mate.h
@@ -3,11 +3,11 @@
 
 // 奇数手詰めチェック
 // 詰ます手を返すバージョン
-template <bool INCHECK = false>
+template <bool INCHECK>
 Move mateMoveInOddPlyReturnMove(Position& pos, const int depth);
 
 // 奇数手詰めチェック
-template <bool INCHECK = false>
+template <bool INCHECK>
 bool mateMoveInOddPly(Position& pos, const int depth);
 
 // 偶数手詰めチェック
